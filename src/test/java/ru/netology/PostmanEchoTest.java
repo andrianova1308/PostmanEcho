683cfc425d6293a6d1c1d292b805749da0c051ce
Анастасия Андрианова <samsikovaa@mail.ru>
2022-09-09 13:46:10 +0300

    Fourth commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -12,6 +12,7 @@ public class PostmanEchoTest {
         // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
                 .body("данные") // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
